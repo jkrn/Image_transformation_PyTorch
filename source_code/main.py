4b5e907178b52233040040bd142cd1780b79104b
jkrn <111745714+jkrn@users.noreply.github.com>
2022-08-23 14:05:07 +0200

    Update main.py

--- source_code/main.py
+++ source_code/main.py
@@ -1,10 +1,10 @@
 from PIL import Image
-import matplotlib.pyplot as plt
 import numpy as np
-
 import torch
 import torchvision.transforms as T
 from torchvision.utils import save_image
+
+# --- PARAMETERS ---
 
 # Image names
 input_image_name = 'input.jpg'
@@ -14,19 +14,19 @@ output_image_name = 'output.png'
 kernel_length = 15
 sigma_value = 9
 
-# Read image
-orig_img = Image.open(input_image_name)
+# --- MAIN ---
 
-# Compose transforms
-transforms = T.Compose([
-    T.ToTensor() ,
-    T.Grayscale(1) ,
-    T.Normalize(0, 1, inplace=False) ,
-    T.GaussianBlur(kernel_size=(kernel_length, kernel_length), sigma=(sigma_value, sigma_value))
-])
-
-# Apply transforms
-tensor_img = transforms(orig_img)
-
-# Save output image
-save_image(tensor_img, output_image_name)
+if __name__ == '__main__':
+    # Read image
+    orig_img = Image.open(input_image_name)
+    # Compose transforms
+    transforms = T.Compose([
+        T.ToTensor() ,
+        T.Grayscale(1) ,
+        T.Normalize(0, 1, inplace=False) ,
+        T.GaussianBlur(kernel_size=(kernel_length, kernel_length), sigma=(sigma_value, sigma_value))
+    ])
+    # Apply transforms
+    tensor_img = transforms(orig_img)
+    # Save output image
+    save_image(tensor_img, output_image_name)
